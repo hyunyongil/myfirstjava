b38649b0693090685796527920025b1ed2fa98f7
hyr <hyunyongil@naver.com>
2020-12-03 09:43:37 +0800

    20201202_1

---
+++ src/YuFa01.java
@@ -0,0 +1,19 @@
+public class YuFa01 {
+    public static void main(String[] args) {
+        byte b1 = 12;
+        byte b2 = -128;
+        System.out.println(b1);
+        System.out.println(b2);
+
+        short s1 = 12;
+        int i1 = 123;
+        long l1 = 1231244L;
+        System.out.println(l1);
+
+        double d1 = 234.54;
+        System.out.println(d1+1);
+
+        float f1 = 12.3F;
+        System.out.println(f1);
+    }
+}
