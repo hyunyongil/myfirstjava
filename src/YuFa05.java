82264bffca1f688c6d3e6ae2bb6b602f37e6d1ab
hyr <hyunyongil@naver.com>
2020-12-07 12:58:09 +0800

    20201207_2

--- src/YuFa05.java
+++ src/YuFa05.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class YuFa05 {
     public static void main(String[] args) {
-        int number = 2;
+/*        int number = 2;
         switch (number){
             case 0:
                 System.out.println("zero");
@@ -16,6 +18,39 @@ public class YuFa05 {
                 break;
             default:
                 System.out.println("other");
+        }*/
+        Scanner scan = new Scanner(System.in);
+        System.out.print("请输入月数:");
+        int month = scan.nextInt();
+        System.out.print("请输入日数:");
+        int day = scan.nextInt();
+        int allDay = 0;
+        switch (month){
+            case 12:
+                allDay += 30;
+            case 11:
+                allDay += 31;
+            case 10:
+                allDay += 30;
+            case 9:
+                allDay += 31;
+            case 8:
+                allDay += 31;
+            case 7:
+                allDay += 30;
+            case 6:
+                allDay += 31;
+            case 5:
+                allDay += 30;
+            case 4:
+                allDay += 31;
+            case 3:
+                allDay += 28;
+            case 2:
+                allDay += 31;
+            case 1:
+                allDay += day;
         }
+        System.out.println(allDay);
     }
 }
