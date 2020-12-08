9b2bc7bea4b9a96ba1da0c27e9e3014b93067c35
hyr <hyunyongil@naver.com>
2020-12-08 13:07:56 +0800

    20201208_2

---
+++ src/YuFaForFor08.java
@@ -0,0 +1,22 @@
+public class YuFaForFor08 {
+    public static void main(String[] args) {
+/*        for (int j=1;j<=10;j++){
+            for (int i=1;i<=11-j;i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+/*        int num = 9;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                String str = " ";
+                if (i == j) {
+                    str = "";
+                }
+                System.out.print(i + "*" + j + "=" + i * j + str);
+            }
+            System.out.println();
+        }*/
+
+    }
+}
--- src/YuFaWhile07.java
+++ src/YuFaWhile07.java
@@ -1,19 +1,44 @@
+import java.util.Scanner;
+
 public class YuFaWhile07 {
     public static void main(String[] args) {
-        int i = 50;
+/*        int i = 50;
         while (i <= 100) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
             i++;
-        }
+        }*/
 
-        int j = 60;
+/*        int j = 1;
+        int ct = 0;
+        int sum = 0;
         do {
             if (j % 2 == 0) {
+                sum += j;
+                ct++;
                 System.out.println(j);
             }
             j++;
         } while (j <= 100);
+        System.out.println(sum);
+        System.out.println(ct);*/
+        Scanner scan = new Scanner(System.in);
+        int ct1 = 0;
+        int ct2 = 0;
+        //while (true) {
+        for (;;){
+            int number = scan.nextInt();
+            if (number == 0) {
+                break;
+            }
+            if (number > 0) {
+                ct1++;
+            } else {
+                ct2++;
+            }
+        }
+        System.out.println("정수는:" + ct1);
+        System.out.println("부수는:" + ct2);
     }
 }
