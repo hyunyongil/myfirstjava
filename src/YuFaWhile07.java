824b2b1a6c389ca2ca0606dde21ee8e1f2d50738
hyr <hyunyongil@naver.com>
2020-12-08 12:14:57 +0800

    20201208_1

--- src/YuFaWhile07.java
+++ src/YuFaWhile07.java
@@ -1,11 +1,19 @@
 public class YuFaWhile07 {
     public static void main(String[] args) {
-        int i = 2;
-        while (i <= 100){
-            if(i % 2 == 0){
+        int i = 50;
+        while (i <= 100) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
             i++;
         }
+
+        int j = 60;
+        do {
+            if (j % 2 == 0) {
+                System.out.println(j);
+            }
+            j++;
+        } while (j <= 100);
     }
 }
