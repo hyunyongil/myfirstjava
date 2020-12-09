49c341d45a01cb78685e9c1336745490f380e485
hyr <hyunyongil@naver.com>
2020-12-09 08:17:44 +0800

    20201208_4

--- src/FamilyAccount.java
+++ src/FamilyAccount.java
@@ -1,6 +1,15 @@
 public class FamilyAccount {
     public static void main(String[] args) {
-
+        boolean isFlag = true;
+        while (isFlag){
+            System.out.println("------가계부명세-------");
+            System.out.println("            1. 수입지출명세");
+            System.out.println("            2. 수입추가");
+            System.out.println("            3. 지출추가");
+            System.out.println("            4. 나가기");
+            System.out.print("              1부터4 사이에서 선택해주세요:");
+            isFlag = false;
+        }
     }
 }
 
---
+++ src/YuFaArray09.java
@@ -0,0 +1,5 @@
+public class YuFaArray09 {
+    public static void main(String[] args) {
+
+    }
+}
