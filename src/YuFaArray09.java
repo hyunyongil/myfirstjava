a028540fdecbb3f4a43a0d02bdef697af37d9a39
hyr <hyunyongil@naver.com>
2020-12-09 12:29:01 +0800

    20201209_

--- src/YuFaArray09.java
+++ src/YuFaArray09.java
@@ -1,5 +1,19 @@
 public class YuFaArray09 {
     public static void main(String[] args) {
+        int[] ids;
+        ids = new int[]{1001, 1002, 1003, 1004};
+        String[] names = new String[5];
+/*        int[] dd = new int[4];
+        names[0] = "ab";
+        names[1] = "cd";
+        names[2] = "ef";
+        names[3] = "gh";
+        names[4] = "ij";
+        char name1 = names[2].charAt(0);
+        System.out.println(name1);*/
 
+        for(int i = 0; i < names.length; i++) {
+          System.out.println(names[i]);
+        }
     }
 }
