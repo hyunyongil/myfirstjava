465b35a4aa9ae83b9e0269e213764925df95feb2
hyr <hyunyongil@naver.com>
2020-12-09 13:53:12 +0800

    20201209_03

--- src/YuFaArrays10.java
+++ src/YuFaArrays10.java
@@ -5,8 +5,11 @@
  */
 public class YuFaArrays10 {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3};
-        int[][] arr1 = new int[][]{{1, 2, 3}, {4, 5, 6}};
+        //int[] arr = new int[]{1, 2, 3};
+        int[] arrs = {1, 2, 3};
+        //int[][] arr1 = new int[][]{{1, 2, 3}, {4, 5, 6}};
+        int[][] arr1 = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(arr1[1][2]);
         String[][] arr2 = new String[3][];
 
     }
