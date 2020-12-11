45be9024bb906339d62a059d470614e39f276aba
hyr <hyunyongil@naver.com>
2020-12-11 16:13:22 +0800

    20201211_06

--- src/object/Oop01.java
+++ src/object/Oop01.java
@@ -7,6 +7,34 @@ package object;
  */
 public class Oop01 {
     public static void main(String[] args) {
-        
+        Phone p = new Phone();
+        System.out.println(p);
+        p.sendEmail();
+        p.playGame();
+        //匿名对象
+        new Phone().price = 1999;
+        new Phone().showPrice();
+        PhoneMall mall = new PhoneMall();
+        mall.show(new Phone());
+    }
+}
+
+class PhoneMall{
+    public void show(Phone phone){
+        phone.sendEmail();
+        phone.playGame();
+    }
+}
+
+class Phone {
+    double price;
+    public void sendEmail(){
+        System.out.println("메일발송");
+    }
+    public void playGame(){
+        System.out.println("게임하기");
+    }
+    public void showPrice(){
+        System.out.println("핸드폰 가격은: "+price);
     }
 }
